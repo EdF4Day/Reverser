edb8323aac6b3978ae6b31a2e9ad58a0cd4c1c9e
Ed Fallin <EFallin@midmark.com>
2021-09-07 13:19:40 -0700

    TDD of ReversalParser and ContentChange started.

--- Reverser/ContentChange.cs
+++ Reverser/ContentChange.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+#pragma warning disable
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +11,40 @@ namespace Reverser
 {
     public class ContentChange
     {
-        List<string> Files { get; set; }
-        bool IsRegex { get; set; }
-        string From { get; set; }
-        string To { get; set; }
+        public List<string> Files { get; set; }
+        public bool IsRegex { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+
+        public ContentChange()
+        {
+            /* No operations. */
+        }
+
+        public ContentChange(List<string> files, bool isRegex, string from, string to)
+        {
+            Files = files;
+            IsRegex = isRegex;
+            From = from;
+            To = to;
+        }
+
+        public override bool Equals(object obj)  /* passed */ 
+        {
+            // Is a ContentChange.  Objects equal if all values equal.
+            if (obj is ContentChange that)
+            {
+                bool filesAreEqual = this.Files.SequenceEqual(that.Files);
+
+                return filesAreEqual 
+                    && this.IsRegex == that.IsRegex 
+                    && this.From == that.From 
+                    && this.To == that.To
+                    ;
+            }
+
+            // Not a ContentChange.
+            return false;
+        }
     }
 }
---
+++ ReverserTests/ReversalParserTests.cs
@@ -0,0 +1,83 @@
+﻿
+#pragma warning disable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Reverser;
+
+namespace ReverserTests
+{
+    [TestClass()]
+    public class ReversalParserTests
+    {
+        [TestMethod()]
+        public void Experiment__ContentChange__SameContents__AreEqual()  /* working */ 
+        {
+            //**  Arrange.  **//
+            ContentChange left = new ContentChange
+            {
+                Files = new List<string> { "a", "b", "t" },
+                IsRegex = true,
+                From = "abc zyx",
+                To = "xyz cba"
+            };
+
+            ContentChange right = new ContentChange
+            {
+                Files = new List<string> { "a", "b", "t" },
+                IsRegex = true,
+                From = "abc zyx",
+                To = "xyz cba"
+            };
+
+
+            //**  Act.  **//
+
+
+            //**  Assert.  **//
+            Assert.AreEqual(left, right);
+        }
+
+
+        [TestMethod()]
+        public void ParseToChanges__KnownSingleChangeBlock__CorrectOutputChangeObject()  /* working */ 
+        {
+            //**  Arrange.  **//
+            ReversalParser target = new ReversalParser();
+            string source =
+@"
+File/s:
+X:\Some\Path\Reversible.txt
+
+IsRegex: true
+From: abc
+To: xyz
+";
+
+
+            //**  Act.  **//
+            List<ContentChange> actuals = target.ParseToChanges(source);
+
+
+            //**  Assert.  **//
+            ContentChange expected = new ContentChange()
+            {
+                Files = new List<string> { @"X:\Some\Path\Reversible.txt" },
+                IsRegex = true,
+                From = "abc",
+                To = "xyz"
+            };
+
+            List<ContentChange> expecteds = new List<ContentChange> { expected };
+
+            CollectionAssert.AreEquivalent(expecteds, actuals);
+        }
+
+    }
+}
