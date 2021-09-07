5c9a2e19415a472ea5a2fc8dff21f36c3a7195ef
Ed Fallin <EFallin@midmark.com>
2021-09-07 12:41:56 -0700

    Started to code change end.

---
+++ Reverser/ContentChange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverser
+{
+    public class ContentChange
+    {
+        List<string> Files { get; set; }
+        bool IsRegex { get; set; }
+        string From { get; set; }
+        string To { get; set; }
+    }
+}
--- Reverser/Properties/Settings.Designer.cs
+++ Reverser/Properties/Settings.Designer.cs
@@ -8,21 +8,31 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace Reverser.Properties
-{
+namespace Reverser.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("File/s:\r\nC:\\Users\\Ed\\Desktop\\Reversible.txt\r\n\r\nIsRegex: false\r\nFrom: abc\r\nTo: xyz" +
+            "\r\n")]
+        public string Reversings {
+            get {
+                return ((string)(this["Reversings"]));
+            }
+            set {
+                this["Reversings"] = value;
             }
         }
     }
