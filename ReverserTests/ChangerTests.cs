f815ea8998ae258117408dc6e9ca51ef29ec63ab
Ed Fallin <EFallin@midmark.com>
2021-09-08 13:49:22 -0700

    More tests of Changer code.  All those tests passed.  Regression tested, all tests passed.

--- ReverserTests/ChangerTests.cs
+++ ReverserTests/ChangerTests.cs
@@ -16,14 +16,15 @@ namespace ReverserTests
     [TestClass()]
     public class ChangerTests
     {
+        #region ChangeForward()
+
         [TestMethod()]
-        public void ChangeForward__NoSuchFile__DoesNotFail()  /* working */ 
+        public void ChangeForward__NoSuchFile__DoesNotFail()  /* working */
         {
             //**  Arrange.  **//
             Mock<IFileStore> _storeMocker = new Mock<IFileStore>();
             _storeMocker.Setup(x => x.Exists(It.IsAny<string>()))
                 .Returns(false);
-
 
             Changer target = new Changer(_storeMocker.Object);
 
@@ -50,8 +51,130 @@ namespace ReverserTests
 
 
             //**  Assert.  **//
-            Assert.IsNull(actual);
+            Assert.AreEqual(null, actual);
         }
+
+        [TestMethod()]
+        public void ChangeForward__SingleFileNotRegex__SwapsFromForTo()  /* working */ 
+        {
+            //**  Arrange.  **//
+            string file = "Some-File";
+            string input = "a b c d a b c d a";
+            string output = "z b c d z b c d z";
+
+            Mock<IFileStore> _storeMocker = new Mock<IFileStore>();
+
+            _storeMocker.Setup(x => x.Exists(It.IsAny<string>()))
+                .Returns(true);
+            _storeMocker.Setup(x => x.Read(It.IsAny<string>()))
+              .Returns(input);
+
+            // Test condition.
+            _storeMocker.Setup(x => x.Write(It.IsAny<string>(), It.Is<string>(arg => arg == output)))
+                .Verifiable();
+
+            Changer target = new Changer(_storeMocker.Object);
+
+            ContentChange change = new ContentChange
+            {
+                Files = new List<string> { @"X:\No\Such\File\Or\Even\Path\Exists\At\All.txt" },
+                IsRegex = false,
+                From = "a",
+                To = "z"
+            };
+
+
+            //**  Act.  **//
+            target.ChangeForward(change);
+
+
+            //**  Assert.  **//
+            _storeMocker.Verify();
+        }
+
+        #endregion ChangeForward()
+
+
+        #region ChangeBack()
+
+        [TestMethod()]
+        public void ChangeBack__NoSuchFile__DoesNotFail()  /* working */
+        {
+            //**  Arrange.  **//
+            Mock<IFileStore> _storeMocker = new Mock<IFileStore>();
+            _storeMocker.Setup(x => x.Exists(It.IsAny<string>()))
+                .Returns(false);
+
+            Changer target = new Changer(_storeMocker.Object);
+
+            ContentChange change = new ContentChange
+            {
+                Files = new List<string> { @"X:\No\Such\File\Or\Even\Path\Exists\At\All.txt" },
+                IsRegex = false,
+                From = "x",
+                To = "y"
+            };
+
+            Exception actual = null;
+
+
+            //**  Act.  **//
+            try
+            {
+                target.ChangeBack(change);
+            }
+            catch (Exception thrown)
+            {
+                actual = thrown;
+            }
+
+
+            //**  Assert.  **//
+            Assert.AreEqual(null, actual);
+        }
+
+        [TestMethod()]
+        public void ChangeBack__SingleFileNotRegex__SwapsToForFrom()  /* working */ 
+        {
+            //**  Arrange.  **//
+            string file = "Some-File";
+
+            // Test condition: "a" to "z" is reversed, from "z" to "a".
+            string input = "z b c d z b c d z";
+            string output = "a b c d a b c d a";
+
+            Mock<IFileStore> _storeMocker = new Mock<IFileStore>();
+
+            _storeMocker.Setup(x => x.Exists(It.IsAny<string>()))
+                .Returns(true);
+            _storeMocker.Setup(x => x.Read(It.IsAny<string>()))
+              .Returns(input);
+
+            _storeMocker.Setup(x => x.Write(It.IsAny<string>(), It.Is<string>(arg => arg == output)))
+                .Verifiable();
+
+            Changer target = new Changer(_storeMocker.Object);
+
+            ContentChange change = new ContentChange
+            {
+                Files = new List<string> { @"X:\No\Such\File\Or\Even\Path\Exists\At\All.txt" },
+                IsRegex = false,
+
+                // These should be used in reverse.
+                From = "a",
+                To = "z"
+            };
+
+
+            //**  Act.  **//
+            target.ChangeBack(change);
+
+
+            //**  Assert.  **//
+            _storeMocker.Verify();
+        }
+
+        #endregion ChangeBack()
 
     }
 }
