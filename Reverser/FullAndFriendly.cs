0a7a949d3f70486f91cae3d4269494a63ebdc102
Ed Fallin <EFallin@midmark.com>
2021-12-27 09:02:25 -0800

    Added a type to allow keeping file names and full paths together, one for display, one for file usage.  This type is not yet in use.

---
+++ Reverser/FullAndFriendly.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverser
+{
+    public class FullAndFriendly
+    {
+        public string FullPath { get; set; }
+        public string FriendlyName { get; set; }
+    }
+}
